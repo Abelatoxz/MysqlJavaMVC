69ab36c1767df41626b766f9837ab4df8e77c03c
Abelinux <92982490+Abelatoxz@users.noreply.github.com>
2024-06-07 18:54:38 +0200

    Update Conexion.java

--- src/Utilidad/Conexion.java
+++ src/Utilidad/Conexion.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
+    //Mariadb Driver
     private static final String URL = "jdbc:mariadb://gitlab.abelflix.es:9000/Bibloteca";
     private static final String USUARIO = "bibloteca";
     private static final String CONTRASENA = "password";
